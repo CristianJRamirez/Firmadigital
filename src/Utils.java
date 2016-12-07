17a83cbdb47a2903613d4c9ce4d6f4ea5767cb8d
CristianJRamirez <45858000w@iespoblenou.org>
2016-12-07 17:42:05 +0100

    Initial commit

---
+++ src/Utils.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+
+/**
+ * Created by 45858000w on 07/12/16.
+ */
+public class Utils {
+    public static boolean areKeysPresent() {
+        return true;
+    }
+
+    public static KeyPair generateKey() {
+        return null;
+    }
+
+    public static byte[] digestiona(File f, String md5) {
+        return null;
+    }
+
+    public static byte[] signar(byte[] digestionat, PrivateKey prik) {
+        return null;
+    }
+
+    public static void read(File f) {
+    }
+
+    public static void concatenateByteArrays(void read, byte[] encryptdigestionat) {
+    }
+
+    public static void write(String fitxerSignat, void aVoid) {
+    }
+}
