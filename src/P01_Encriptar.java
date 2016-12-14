d5e2923338606961ca2af7787684c1752438546e
CristianJRamirez <45858000w@iespoblenou.org>
2016-12-14 17:41:56 +0100

    actualizacion, solo falta hacer la funcion de signar y comprobar todo

--- src/P01_Encriptar.java
+++ src/P01_Encriptar.java
@@ -1,5 +1,3 @@
-import com.sun.org.apache.xpath.internal.SourceTree;
-
 import java.io.*;
 import java.security.*;
 
@@ -10,8 +8,10 @@ public class P01_Encriptar {
 
     public static final String PRIVATE_KEY_FILE = "private.key";
 
-    public static final String FITXER_PLA = "Sparring.pdf";
+    public static final String FITXER_PLA = "UF1-NF1-P01-Signatura.pdf";
     public static final String FITXER_SIGNAT = "firmat.pdf";
+    public static final String ficheroPublico= "Public.key";
+    public static final String ficheroPrivate= "Private.key";
 
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException, ClassNotFoundException {
 
@@ -20,8 +20,8 @@ public class P01_Encriptar {
 
         File f = new File(FITXER_PLA);
 
-        if(!Utils.areKeysPresent()){
-            keyPair = Utils.generateKey();
+        if(!Utils.areKeysPresent(ficheroPublico,ficheroPrivate)){//comprueba si hay archivos private.key y public.key, sino estan las crea
+            keyPair = Utils.generateKey(ficheroPublico,ficheroPrivate);
             prik = keyPair.getPrivate();
         }else{
             ObjectInputStream inputStream = null;
@@ -29,11 +29,13 @@ public class P01_Encriptar {
             prik = (PrivateKey) inputStream.readObject();
         }
 
-        byte[] digestionat = Utils.digestiona(f,"MD5");
-        byte[] encryptdigestionat = Utils.signar(digestionat,prik);
+        byte[] hash = Utils.getCodigoHash(f,"MD5");
+        byte[] encryptHash = Utils.signar(hash,prik);
+
         System.out.println("Longitud del fitxer: "+f.length());
-        System.out.println("Longitud de la firma: "+encryptdigestionat.length);
-        Utils.write(FITXER_SIGNAT,Utils.concatenateByteArrays(Utils.read(f),encryptdigestionat));
+        System.out.println("Longitud de la firma: "+encryptHash.length);
+
+        Utils.write(FITXER_SIGNAT,Utils.concatenateByteArrays(Utils.read(f),encryptHash));
     }
 
 }
--- src/Utils.java
+++ src/Utils.java
@@ -1,33 +1,170 @@
-import java.io.File;
-import java.security.KeyPair;
-import java.security.PrivateKey;
+import java.io.*;
+import java.math.BigInteger;
+import java.security.*;
 
 /**
  * Created by 45858000w on 07/12/16.
  */
 public class Utils {
-    public static boolean areKeysPresent() {
-        return true;
+
+    /**
+     * comprueba si existen los archivos private.key y public.key
+     * @return
+     * @param ficheroPublico
+     * @param ficheroPrivate
+     */
+    public static boolean areKeysPresent(String ficheroPublico, String ficheroPrivate) {
+        File filePub= new File(ficheroPublico);
+        File filePriv= new File(ficheroPrivate);
+        if(filePub.exists())
+        {
+            if(filePriv.exists())
+            {
+                System.out.println("Existen los dos archivos");
+                return true;
+            }
+            else
+            {
+                System.out.println("No existe el archivo de Private.Key");
+                return false;
+            }
+        }
+        else
+        {
+            if(filePriv.exists())
+            {
+                System.out.println("No existe el archivo de Public.Key");
+                return false;
+            }
+            else
+            {
+                System.out.println("No existen ninguno de los archivos");
+                return false;
+            }
+        }
+
     }
 
-    public static KeyPair generateKey() {
+    /**
+     * generar les claus públiques i les claus privades
+     * @return
+     * @param ficheroPublico
+     * @param ficheroPrivate
+     */
+    public static KeyPair generateKey(String ficheroPublico, String ficheroPrivate) throws IOException {
+        KeyPair clave = null;//La clase KeyPair soporta una clave privada y una pública.
+        try {
+//Usamos el algoritmo RSA (RSA es un sistema criptográfico de clave pública desarrollado en 1977).
+            KeyPairGenerator generador = KeyPairGenerator.getInstance("RSA");
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+            generador.initialize(1024);//Tamaño de la clave.
+
+            clave = generador.genKeyPair();
+
+            System.out.println("Clave privada: "+clave.getPrivate().toString());
+            System.out.println("Clave pública: "+clave.getPublic().toString());
+
+
+            File publicKeyFile= new File(ficheroPublico);
+            if (publicKeyFile.getParentFile()==null)
+            {
+                publicKeyFile.getParentFile();
+            }
+            publicKeyFile.createNewFile();
+            ObjectOutputStream pub = new ObjectOutputStream(new FileOutputStream(publicKeyFile));
+            pub.writeObject(clave.getPublic());
+
+            File privateKeyFile= new File(ficheroPrivate);
+            if (privateKeyFile.getParentFile()==null)
+            {
+                privateKeyFile.getParentFile();
+            }
+            privateKeyFile.createNewFile();
+            ObjectOutputStream priv = new ObjectOutputStream(new FileOutputStream(privateKeyFile));
+            priv.writeObject(clave.getPublic());
+
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return clave;
+    }
+
+    /**
+     * obtenir el HASH d'un fitxer. fer servir l'algoritme MD5
+     * Per a encriptar el hash emprarem l'algoritme RSA (però guardeu el nom de l'algoritme en una variable global per si es volgués canviar)
+     * @param f direccion del archivo
+
+     * @return
+     */
+    public static byte[] getCodigoHash(File f, String md5) throws NoSuchAlgorithmException, IOException {
+        {
+            MessageDigest digest = MessageDigest.getInstance(md5);
+            InputStream is = new FileInputStream(f);
+            byte[] buffer = new byte[(int) f.length()];
+            int read = 0;
+            while ((read = is.read(buffer)) > 0)
+            {
+                digest.update(buffer, 0, read);
+            }
+            byte[] md5sum = digest.digest();
+            BigInteger bigInt = new BigInteger(1, md5sum);
+            String output = bigInt.toString(16);
+            is.close();
+            //return output;
+            return md5sum;
+        }
+    }
+
+
+    public static byte[] signar(byte[] hash, PrivateKey prik) {
         return null;
     }
 
-    public static byte[] digestiona(File f, String md5) {
-        return null;
+    /**
+     * La funció read passar un fitxer a un array de bytes.
+     * @param f
+     */
+    public static byte[] read(File f) throws IOException {
+        FileInputStream ficheroStream = new FileInputStream(f);
+        byte contenido[] = new byte[(int)f.length()];
+        ficheroStream.read(contenido);
+        return contenido;
     }
 
-    public static byte[] signar(byte[] digestionat, PrivateKey prik) {
-        return null;
+    /**
+     * concatenar los dos bytes
+     * @param fitxerLectura
+     * @param encryptHash
+     * @return
+     */
+    public static byte[] concatenateByteArrays(byte[] fitxerLectura, byte[] encryptHash) {
+        byte concatenado[] = new byte[fitxerLectura.length+encryptHash.length];
+        System.arraycopy(fitxerLectura, 0, concatenado, 0, fitxerLectura.length);
+        System.arraycopy(encryptHash, 0, concatenado, fitxerLectura.length, encryptHash.length);
+        return concatenado;
     }
 
-    public static void read(File f) {
+
+    /**
+     * La funció write guarda un array de bytes en un fitxer.
+     * @param fitxerSignat
+     * @param byteArray
+     */
+    public static void write(String fitxerSignat, byte[] byteArray) throws IOException {
+        FileOutputStream fos = new FileOutputStream(fitxerSignat);
+        fos.write(byteArray);
+
     }
 
-    public static void concatenateByteArrays(void read, byte[] encryptdigestionat) {
+
+    public static boolean compararFicheroHash(File file, String hashCode) throws NoSuchAlgorithmException, FileNotFoundException, IOException
+    {
+        return hashCode.equals(getCodigoHash(file,"MD5"));
     }
 
-    public static void write(String fitxerSignat, void aVoid) {
+    public static boolean compararFicheros(File file1, File file2) throws NoSuchAlgorithmException, FileNotFoundException, IOException
+    {
+        return getCodigoHash(file1,"MD5").equals(getCodigoHash(file2,"MD5"));
     }
 }
